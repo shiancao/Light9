c86272d95518100c4ef26cc198d7a91b20d8ba3b
shiancao <89098202+shiancao@users.noreply.github.com>
2021-08-17 18:05:10 +0000

    Update PacketHandler.java

--- src/main/java/me/Danker/handlers/PacketHandler.java
+++ src/main/java/me/Danker/handlers/PacketHandler.java
@@ -2,9 +2,12 @@ package me.Danker.handlers;
 
 import io.netty.channel.ChannelDuplexHandler;
 import io.netty.channel.ChannelHandlerContext;
+import me.Danker.commands.ToggleNoRotateCommand;
 import me.Danker.utils.Utils;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.network.Packet;
+import net.minecraft.network.play.server.S08PacketPlayerPosLook;
 import net.minecraft.network.play.server.S04PacketEntityEquipment;
 
 import java.lang.reflect.Field;
@@ -14,14 +17,21 @@ public class PacketHandler extends ChannelDuplexHandler {
 	// Spirit boots fix
 	@Override
 	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-		if (Utils.inSkyblock && msg instanceof Packet && msg.getClass().getName().endsWith("S04PacketEntityEquipment")) { // Inventory packet name
-			S04PacketEntityEquipment packet = (S04PacketEntityEquipment) msg;
-			if (packet.getEntityID() == Minecraft.getMinecraft().thePlayer.getEntityId()) {
-				Field slot = packet.getClass().getDeclaredField("field_149392_b"); // equipmentSlot
-				slot.setAccessible(true);
-				slot.setInt(packet, slot.getInt(packet) + 1);
-				msg = packet;
-			}
+		if (msg instanceof net.minecraft.network.Packet && msg.getClass().getName().endsWith("S08PacketPlayerPosLook") && ToggleNoRotateCommand.norotatetoggled) {
+            EntityPlayerSP player = (Minecraft.getMinecraft()).thePlayer;
+            if (player != null) {
+                ItemStack item = player.getHeldItem();
+                if ((item != null && item.getDisplayName().contains("Hyperion")) || (item != null && item.getDisplayName().contains("Aspect of the End"))) {
+                    S08PacketPlayerPosLook packet = (S08PacketPlayerPosLook)msg;
+                    Field packetYaw = packet.getClass().getDeclaredField("field_148936_d");
+                    Field packetPitch = packet.getClass().getDeclaredField("field_148937_e");
+                    packetYaw.setAccessible(true);
+                    packetPitch.setAccessible(true);
+                    packetYaw.setFloat(packet, player.rotationYaw);
+                    packetPitch.setFloat(packet, player.rotationPitch);
+                    msg = packet;
+                }
+            }
 		}
 		
 		super.channelRead(ctx, msg);
